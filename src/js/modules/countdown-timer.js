abc9040e84ac05efcca3ff3a9544c225028d1451
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2023-03-23 13:20:21 +0200

    Update countdown-timer.js

--- src/js/modules/countdown-timer.js
+++ src/js/modules/countdown-timer.js
@@ -4,7 +4,25 @@ import 'jquery-countdown';
 export function cuontdownTimer(){
   $('#getting-started').countdown(
     '2023/01/01', function (event) {
-      $(this).html(event.strftime('<ul class="hero__time-list"><li class="hero__time-list-item"><p class="countdown-time">%D</p><p class="countdown-text">Дней</p></li><li class="hero__time-list-item"><p class="countdown-time">%H</p><p class="countdown-text">Часов</p></li><li class="hero__time-list-item"><p class="countdown-time">%M</p><p class="countdown-text">Минут</p></li><li class="hero__time-list-item"><p class="countdown-time">%S</p><p class="countdown-text">Секунд</p></li></ul>'));
+      $(this).html(event.strftime(
+        `<ul class="hero__time-list">
+          <li class="hero__time-list-item">
+            <p class="countdown-time">%D</p>
+            <p class="countdown-text">Дней</p>
+          </li>
+          <li class="hero__time-list-item">
+            <p class="countdown-time">%H</p>
+            <p class="countdown-text">Часов</p>
+          </li>
+          <li class="hero__time-list-item">
+            <p class="countdown-time">%M</p>
+            <p class="countdown-text">Минут</p>
+          </li>
+          <li class="hero__time-list-item">
+            <p class="countdown-time">%S</p>
+            <p class="countdown-text">Секунд</p>
+          </li>
+        </ul>`));
     }
   );
 };
